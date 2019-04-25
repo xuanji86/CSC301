233b5a3541cad21c67b33178553c90427a2f3ed7
xuanji86 <39474125+xuanji86@users.noreply.github.com>
2019-04-25 00:46:41 -0500

    homework 5

--- src/Point.java
+++ src/Point.java
@@ -1,12 +1,3 @@
-/******************************************************************************
- *  Compilation:  javac Point.java
- *  Execution:    java Point
- *  Dependencies: none
- *  
- *  An immutable data type for points in the plane.
- *  For use on Coursera, Algorithms Part I programming assignment.
- *
- ******************************************************************************/
 
 import java.util.Comparator;
 import edu.princeton.cs.algs4.StdDraw;
@@ -19,8 +10,8 @@ public class Point implements Comparable<Point> {
     /**
      * Initializes a new point.
      *
-     * @param  x the <em>x</em>-coordinate of the point
-     * @param  y the <em>y</em>-coordinate of the point
+     * @param x the <em>x</em>-coordinate of the point
+     * @param y the <em>y</em>-coordinate of the point
      */
     public Point(int x, int y) {
         /* DO NOT MODIFY */
@@ -37,8 +28,8 @@ public class Point implements Comparable<Point> {
     }
 
     /**
-     * Draws the line segment between this point and the specified point
-     * to standard draw.
+     * Draws the line segment between this point and the specified point to
+     * standard draw.
      *
      * @param that the other point
      */
@@ -48,18 +39,27 @@ public class Point implements Comparable<Point> {
     }
 
     /**
-     * Returns the slope between this point and the specified point.
-     * Formally, if the two points are (x0, y0) and (x1, y1), then the slope
-     * is (y1 - y0) / (x1 - x0). For completeness, the slope is defined to be
-     * +0.0 if the line segment connecting the two points is horizontal;
-     * Double.POSITIVE_INFINITY if the line segment is vertical;
-     * and Double.NEGATIVE_INFINITY if (x0, y0) and (x1, y1) are equal.
+     * Returns the slope between this point and the specified point. Formally,
+     * if the two points are (x0, y0) and (x1, y1), then the slope is (y1 - y0)
+     * / (x1 - x0). For completeness, the slope is defined to be +0.0 if the
+     * line segment connecting the two points is horizontal;
+     * Double.POSITIVE_INFINITY if the line segment is vertical; and
+     * Double.NEGATIVE_INFINITY if (x0, y0) and (x1, y1) are equal.
      *
-     * @param  that the other point
+     * @param that the other point
      * @return the slope between this point and the specified point
      */
     public double slopeTo(Point that) {
-        /* YOUR CODE HERE */
+        if (that.x == this.x) {
+            if (that.y == this.y) {
+                return Double.NEGATIVE_INFINITY;
+            }
+            return Double.POSITIVE_INFINITY;
+        }
+        if(that.y == this.y){
+            return 0;
+        }
+        return(double)(that.y-this.y)/(that.x-this.x);
     }
 
     /**
@@ -67,32 +67,34 @@ public class Point implements Comparable<Point> {
      * Formally, the invoking point (x0, y0) is less than the argument point
      * (x1, y1) if and only if either y0 < y1 or if y0 = y1 and x0 < x1.
      *
-     * @param  that the other point
-     * @return the value <tt>0</tt> if this point is equal to the argument
-     *         point (x0 = x1 and y0 = y1);
-     *         a negative integer if this point is less than the argument
-     *         point; and a positive integer if this point is greater than the
-     *         argument point
+     * @param that the other point
+     * @return the value <tt>0</tt> if this point is equal to the argument point
+     * (x0 = x1 and y0 = y1); a negative integer if this point is less than the
+     * argument point; and a positive integer if this point is greater than the
+     * argument point
      */
     public int compareTo(Point that) {
-        /* YOUR CODE HERE */
+        if (this.y == that.y && this.x == that.x)
+            return 0;
+        if ( this.y < that.y || (this.y == that.y && this.x < that.x))
+            return -1;
+        return 1;
     }
 
     /**
-     * Compares two points by the slope they make with this point.
-     * The slope is defined as in the slopeTo() method.
+     * Compares two points by the slope they make with this point. The slope is
+     * defined as in the slopeTo() method.
      *
      * @return the Comparator that defines this ordering on points
      */
     public Comparator<Point> slopeOrder() {
-        /* YOUR CODE HERE */
+        
     }
 
-
     /**
-     * Returns a string representation of this point.
-     * This method is provide for debugging;
-     * your program should not rely on the format of the string representation.
+     * Returns a string representation of this point. This method is provide for
+     * debugging; your program should not rely on the format of the string
+     * representation.
      *
      * @return a string representation of this point
      */
