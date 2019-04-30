f7d0fae7d4872c9f323836a6c397ff3ecb8ee986
xuanji86 <xuanji86@gmail.com>
2019-04-30 02:38:29 -0500

    debug

--- src/colinearPoint/BruteCollinearPoints.java
+++ src/colinearPoint/BruteCollinearPoints.java
@@ -9,16 +9,20 @@ import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
 
 import java.util.Arrays;
+import org.w3c.dom.Node;
 
 /**
  *
  * @author Anji Xu
  */
 public class BruteCollinearPoints {
+    private int lineNumber;
+    private Node last;
     public BruteCollinearPoints(Point[] points){
         if(points == null){
             throw new NullPointerException();
         }
+        lineNumber = 0;
         int num = points.length;
         Arrays.sort(points);
         for(int i = 0;i < num; i++){
@@ -30,8 +34,8 @@ public class BruteCollinearPoints {
                         Point r = points[k];
                         Point s = points[l];
                         if(p.slopeTo(q) == p.slopeTo(r) && p.slopeTo(q) == p.slopeTo(s)){
-                            StdOut.println(p+" -> "+ q + "->" + r + "->" + s);
-                            p.drawTo(s);
+
+                            lineNumber++;
                         }
                     }
                 }
@@ -39,8 +43,13 @@ public class BruteCollinearPoints {
         }
 
     }
-    
-    
+    public int numberOfSegments(){
+        return lineNumber;
+    }
+    public LineSegment[] segments(){
+        LineSegment[] lines = new LineSegment[lineNumber];
+        
+    }
     
 
 
