fb6de2d6fbf9082010300bbd143295cd406bfd07
xuanji86 <39474125+xuanji86@users.noreply.github.com>
2019-05-01 23:33:21 -0500

    inversion

--- src/anjixu/chapter2/section2/inversion.java
+++ src/anjixu/chapter2/section2/inversion.java
@@ -11,4 +11,45 @@ package anjixu.chapter2.section2;
  */
 public class inversion {
     
+    private inversion(){}
+
+    private static long merge(int[] a, int[] aux, int lo, int mid, int hi) {
+        long inver= 0;
+        
+        for (int k = lo; k <= hi; k++){
+            aux[k] = a[k];
+        }
+        int i = lo;
+        int j = mid+1;
+        for(int k = lo; k<= hi; k++){
+            if(i>mid){
+                a[k] = aux[j++];
+            }
+            else if(j>mid){
+                a[k] = aux[i++];
+            }
+            else if(aux[j]< aux[i]){
+                a[k] = aux[j++];
+                inver = (mid-i+1)+inver;
+            }
+            else{
+                a[k] = aux[i++];
+            }
+        }
+        return inver;
+    }
+    
+    private static long count(int[] a, int[] b, int[] aux, int lo, int hi) {
+        long inver = 0;
+        if(hi<=lo){
+            return 0;
+        }
+        int mid = lo + (hi-lo)/2;
+        inver = count(a,b,aux,lo,mid) + inver;
+        inver = count(a,b,aux,mid+1,hi) + inver;
+        inver = merge(b,aux,lo,mid,hi);
+        assert inver == brute(a,lo,hi);
+        return inver;
+    }
+    
 }
